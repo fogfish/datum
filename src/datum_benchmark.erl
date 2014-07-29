d602a0abdd89ee859597e98bc0adbdb0866f9b0d
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2014-07-30 00:25:30 +0300

    ring benchmark

--- src/datum_benchmark.erl
+++ src/datum_benchmark.erl
@@ -4,6 +4,14 @@
    new/1, run/4
 ]).
 
+-define(N,    512).
+-define(RING, [
+   {m,     16}
+  ,{q,   4096}
+  ,{n,      3}
+  ,{hash, sha}
+]).
+
 %%%------------------------------------------------------------------
 %%%
 %%% factory
@@ -16,7 +24,10 @@ new(_) ->
 %%
 %% datum structure
 init(bst)    ->  {bst,    bst:new()};
-init(rbtree) ->  {rbtree, bst:new()};
+init(rbtree) ->  {rbtree, rbtree:new()};
+init(chord)  ->  {chord,  ring(chord)};
+init(ring)   ->  {ring,   ring(ring)};
+
 %%
 %% erlang built-in data types
 init(dict)     -> {dict,  dict:new()};
@@ -43,12 +54,48 @@ run(lookup, KeyGen, _ValGen, {bst, S0}) ->
 %%%------------------------------------------------------------------
 
 run(insert, KeyGen, ValGen, {rbtree, S0}) ->
-   S1 = bst:insert(KeyGen(), ValGen(), S0),
+   S1 = rbtree:insert(KeyGen(), ValGen(), S0),
    {ok, {rbtree, S1}};
 
 run(lookup, KeyGen, _ValGen, {rbtree, S0}) ->
-   _ = bst:lookup(KeyGen(), S0),
+   _ = rbtree:lookup(KeyGen(), S0),
    {ok, {rbtree, S0}};
+
+%%%------------------------------------------------------------------
+%%%
+%%% chord
+%%%
+%%%------------------------------------------------------------------
+
+run(whereis, _KeyGen, ValGen, {chord, S0}) ->
+   _ = chord:whereis(ValGen(), S0),
+   {ok, {chord, S0}};
+
+run(predecessors, _KeyGen, ValGen, {chord, S0}) ->
+   _ = chord:predecessors(ValGen(), S0),
+   {ok, {chord, S0}};
+
+run(successors,   _KeyGen, ValGen, {chord, S0}) ->
+   _ = chord:successors(ValGen(), S0),
+   {ok, {chord, S0}};
+
+%%%------------------------------------------------------------------
+%%%
+%%% ring
+%%%
+%%%------------------------------------------------------------------
+
+run(whereis, _KeyGen, ValGen, {ring, S0}) ->
+   _ = ring:whereis(ValGen(), S0),
+   {ok, {ring, S0}};
+
+run(predecessors, _KeyGen, ValGen, {ring, S0}) ->
+   _ = ring:predecessors(ValGen(), S0),
+   {ok, {ring, S0}};
+
+run(successors,   _KeyGen, ValGen, {ring, S0}) ->
+   _ = ring:successors(ValGen(), S0),
+   {ok, {ring, S0}};
 
 %%%------------------------------------------------------------------
 %%%
@@ -79,7 +126,6 @@ run(lookup, KeyGen, _ValGen, {gb_trees, S0}) ->
    {ok, {gb_trees, S0}}.
 
 
-
 % run(create, KeyGen, ValGen, {bloom, S0}) ->
 %    S1 = bloom:insert(KeyGen(), S0),
 %    {ok, {bloom,   S1}};
@@ -95,5 +141,30 @@ run(lookup, KeyGen, _ValGen, {gb_trees, S0}) ->
 % run(lookup, KeyGen, ValGen, {bloom, S0}) ->
 %    bloom:lookup(KeyGen(), S0),
 %    {ok, {bloom, S0}}.
+
+
+%%%------------------------------------------------------------------
+%%%
+%%% private
+%%%
+%%%------------------------------------------------------------------
+
+
+ring(Mod) ->
+   io:format("init ring: "),
+   random:seed(os:timestamp()),
+   Ring = lists:foldl(
+      fun(_, Acc) ->
+         io:format("."),
+         Key = list_to_binary(integer_to_list(random:uniform(16#ffffffff))),
+         Val = list_to_binary(integer_to_list(random:uniform(16#ffffffff))),
+         Mod:join(Key, Val, Acc)
+      end,
+      Mod:new(?RING),
+      lists:seq(1, ?N)
+   ),
+   io:nl(),
+   Ring.
+
 
 
