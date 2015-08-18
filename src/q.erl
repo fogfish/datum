bad34a3555f446fe108b98c4a394450fb67b82ec
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2015-08-18 22:26:13 +0300

    split queue

--- src/q.erl
+++ src/q.erl
@@ -33,6 +33,7 @@
   ,length/1
   ,dropwhile/2
   ,takewhile/2
+  ,split/2
   ,splitwith/2
   ,list/1
 ]).
@@ -145,6 +146,23 @@ takewhile(Pred, Acc, {q, _N, _Tail, _Head}=Q) ->
 
 takewhile(_,  Acc, {}) ->
    Acc.
+
+%%
+%% partitions queue into two queues.
+-spec(split/2 :: (function(), datum:q()) -> {datum:q(), datum:q()}).
+
+split(X, {q, N, Tail, Head})
+ when X =< erlang:length(Head) ->
+   {A, B} = lists:split(X, Head),
+   {q:new(A), {q, N - X, Tail, B}};
+   
+split(X, {q, N, Tail, Head})
+ when X < N ->
+   {A, B} = lists:split(erlang:length(Tail) - (X - erlang:length(Head)), Tail),
+   { {q, X, B, Head}, q:new(lists:reverse(A)) };
+   
+split(_, Queue) ->
+   {Queue, q:new()}.
 
 %%
 %% partitions queue into two queues according to predicate.
