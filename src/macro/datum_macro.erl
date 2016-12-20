28631fa66dcd1e78e47ffa569340a0cf601fa132
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2016-12-20 21:15:37 +0200

    add xor category

--- src/macro/datum_macro.erl
+++ src/macro/datum_macro.erl
@@ -29,47 +29,24 @@
 %%%------------------------------------------------------------------   
 
 %%
-%% check if code is category pattern
+%% check if code is category pattern (functional comprehension)
 is_category({char, _, $.}) ->
    function;
+is_category({char, _, $^}) ->
+   'xor';
 is_category(_) ->
    false.
 
 %%
-%% apply category pattern
-category(function, Expr0) ->
-   Expr1 = category_function(Expr0),
-   case is_category_function_partial(Expr1) of
-      true ->
-         category_function_partial_compose(Expr1);
-      false ->
-         category_function_compose(Expr1)
-   end.
-
-%%
-%% built-in functional composition f . g
-category_function([{call, _, _, _} = H | T]) ->
-   [H | category_function(T)];
-category_function([{'fun', Line, {function, Id, _}} | T]) ->
-   [{call, Line, {atom, Line, Id}, [{var, Line, '_'}]} | category_function(T)];
-category_function([{'fun', Line, {function, Mod, Fun, _}}| T]) ->
-   [{call, Line, {remote, Line, Mod, Fun}, [{var, Line, '_'}]} | category_function(T)];
-category_function([H | _]) ->
-   Msg = io_lib:format("The category pattern do not support~n~p~n", [H]),
-   exit(Msg);
-category_function([]) ->
-   [].
-
-%%
-category_function_compose([{call, _, _, _} = F, {call, Line, Gf0, Ga0} | T]) ->
-   Ga1 = set_blank_variable(F, Ga0),
-   G   = {call, Line, Gf0, Ga1},
-   category_function_compose([G|T]);
-category_function_compose([Expr]) ->
+%% apply category composition operator 
+category_compose_with(Fun, [F, {call, _, _, _} = G | T]) ->
+   category_compose_with(Fun, [Fun(F, G)|T]);
+category_compose_with(_, [Expr]) ->
    Expr.
 
 %%
-category_function_partial_compose([{call, Line, Ff0, Fa0}|T]) ->
+%%
+category_compose_partial_with(Fun, [{call, Line, Ff0, Fa0}|T]) ->
    Fa1 = set_blank_variable({var, Line, 'VCatX'}, Fa0),
    G   = {call, Line, Ff0, Fa1},
    {'fun', Line,
@@ -77,18 +54,76 @@ category_function_partial_compose([{call, Line, Ff0, Fa0}|T]) ->
          {clause, Line,
             [{var, Line, 'VCatX'}],
             [],
-            [category_function_compose([G|T])]
+            [category_compose_with(Fun, [G|T])]
          }
       ]}
    }.
 
 %%
-is_category_function_partial([{call, _, _, Fa0} | _]) ->
+%%
+is_category_partial([{call, _, _, Fa0} | _]) ->
    length( lists:filter(fun({var, _, '_'}) -> true; (_) -> false end, Fa0) ) > 0.
 
 
+%%
+%% apply category pattern
+category(function, Expr0) ->
+   Expr1 = function_composition(Expr0),
+   case is_category_partial(Expr1) of
+      true ->
+         category_compose_partial_with(fun compose_function/2, Expr1);
+      false ->
+         category_compose_with(fun compose_function/2, Expr1)
+   end;
 
+category('xor', Expr0) ->
+   Expr1 = function_composition(Expr0),
+   case is_category_partial(Expr1) of
+      true ->
+         {call, Line, _, _} = hd(Expr0),
+         {Var, Expr2} = category_compose_with(fun compose_xor/2, lists:reverse(Expr1)),
+         {'fun', Line,
+            {clauses, [
+               {clause, Line,
+                  [{var, Line, Var}],
+                  [],
+                  [Expr2]
+               }
+            ]}
+         };
+      false ->
+         {_, Expr2} = category_compose_with(fun compose_xor/2, lists:reverse(Expr1)),
+         Expr2
+   end.
 
+%%
+%% compose operator in function category 
+compose_function({call, _, _, _} = F, {call, Line, Gf0, Ga0}) ->
+   %% rewrite f(_) . g(_) to g(f(_)) without runtime overhead 
+   {call, Line, Gf0, set_blank_variable(F, Ga0)}.
+
+%%
+%% compose operator in xor category
+compose_xor({call, Line, Ff0, Fa0}, {call, _, _, _} = G) ->
+   Var = uuid(),
+   compose_xor({Var, {call, Line, Ff0, set_blank_variable({var, Line, Var}, Fa0)}}, G);
+
+compose_xor({V0, F}, {call, Line, Gf0, Ga0}) ->
+   Var = uuid(),
+   Err = uuid(),
+   I = {'case', Line, {call, Line, Gf0, set_blank_variable({var, Line, Var}, Ga0)}, [
+      {clause, Line, 
+         [{tuple, Line, [{atom, Line, ok},{var, Line, V0}]}],
+         [],
+         [F]
+      },
+      {clause, Line,
+         [{match, Line, {tuple,Line, [{atom, Line, error},{var, Line, '_'}]}, {var, Line, Err}}],
+         [],
+         [{var, Line, Err}]
+      }
+   ]},
+   {Var, I}.
 
 %%%------------------------------------------------------------------
 %%%
@@ -97,10 +132,38 @@ is_category_function_partial([{call, _, _, Fa0} | _]) ->
 %%%------------------------------------------------------------------   
 
 %%
+%% unique variable
+uuid() ->
+   list_to_atom("Var" ++ integer_to_list(erlang:unique_integer([monotonic, positive]))).   
+
+
 %%
+%% set blank variable to 
 set_blank_variable(X, [{var, _, '_'}|T]) ->
    [X|set_blank_variable(X, T)];
+
 set_blank_variable(X, [H|T]) ->
    [H|set_blank_variable(X, T)];
+
 set_blank_variable(_, []) ->
    [].  
+
+
+%%
+%% convert expression to function composition (f . g . h ...)
+function_composition([{call, _, _, _} = H | T]) ->
+   [H | function_composition(T)];
+
+function_composition([{'fun', Line, {function, Id, _}} | T]) ->
+   [{call, Line, {atom, Line, Id}, [{var, Line, '_'}]} | function_composition(T)];
+
+function_composition([{'fun', Line, {function, Mod, Fun, _}}| T]) ->
+   [{call, Line, {remote, Line, Mod, Fun}, [{var, Line, '_'}]} | function_composition(T)];
+
+function_composition([H | _]) ->
+   exit( io_lib:format("Function composition do not support the expression:~n~p~n", [H]) );
+
+function_composition([]) ->
+   [].
+
+
