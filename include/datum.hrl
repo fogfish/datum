9d3dda86cbca153be52454596d14c84b56f00592
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2017-02-19 15:02:22 +0200

    (#4) : define NONE macro

--- include/datum.hrl
+++ include/datum.hrl
@@ -6,6 +6,13 @@
 -endif.
 
 %%
-%% xor category pattern match
+%% 
+-define(NONE,  undefined).
+
+%%
+%% either category pattern match
+-define(EITHER_R(X), {ok,    X}).
+-define(EITHER_L(X), {error, X}).
+
 -define(XOR_R(X), {ok,    X}).
 -define(XOR_L(X), {error, X}).
